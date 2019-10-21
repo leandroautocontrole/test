a81452faaf5be1ecc4d89020fd64560b2ea2235b
Leandro Dantas Lima <leandroautocontrole@gmail.com>
2019-10-21 20:44:49 -0300

    copiando arquivo para o github

---
+++ pe8.c
@@ -0,0 +1,84 @@
+ /**************************************************************************
+ *   pe8.c                                    Version 20190817.235856      *
+ *                                                                         *
+ *   Project Euler (MAIOR PRODUTO DE UMA SÉRIE)                            *
+ *   Copyright (C) 2015-2019   Template by Ruben Carlo Benante             *
+ *   Author: Leandro Dantas Lima                                           *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Leandro Dantas Lima                                                   *
+ *   Email: leandroautocontrole@gmail.com                                  *
+ *   Webpage: http://beco.poli.br/leandroautocontrole@gmail.com            *
+ *   Phone: +55 (81) 98861-9469                                            *
+ **************************************************************************/
+
+/* ---------------------------------------------------------------------- */
+/* includes */
+#include "projecteuler.h"
+#include "myeulerlib.h"
+
+/* ---------------------------------------------------------------------- */
+/* defines */
+/* Project Euler Exercise Number */
+/* #define PEN pe ## 8 */
+#define SPEN "pe" "8"
+
+/* ---------------------------------------------------------------------- */
+/* prototypes */
+void pe8(void); /* Proj. Euler problem #8 */
+
+/* ---------------------------------------------------------------------- */
+/* main function */
+/* Do not change the main function. Use the void pe8(void) and
+ * others you create here or in the myeulerlib.c and myeulerlib.h files
+ */
+int main(void)
+{
+    printf("Project Euler 20190817.233439, %s\n", SPEN);
+    help(); /* optional help message at start */
+    GRABsignals(); /* grab interruption signals */
+    pe8(); /* Proj. Euler problem #8 */
+    LEGOsignals(); /* restore interruption signals */
+    return EXIT_SUCCESS;
+}
+
+/* ---------------------------------------------------------------------- */
+/* Proj. Euler problem #8 */
+/* Problem description:
+ *
+ */
+
+void pe8(void)
+{
+    /* local variables */
+    ulong r=0; /* answer */
+
+    /* Put this command inside a loop to get previews when pressing CTRL-C */
+    /* ctrlc_printf("%s: Trying now %u\n", __FUNCTION__, r); */
+
+    /* Use to count minutes running: */
+    /* if(minutes() >= 2) break; */
+
+    printf("%s: %lu\n", __FUNCTION__, r); /* answer */
+    return;
+}
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
+
