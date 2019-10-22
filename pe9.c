8638ee55560f024d1d3ca32bb0d92699db6df776
Leandro Dantas Lima <leandroautocontrole@gmail.com>
2019-10-21 21:28:59 -0300

    copiando arquivo para o github

---
+++ pe9.c
@@ -0,0 +1,47 @@
+ /**************************************************************************
+ *   pe9.c                                    Version 20190817.235856      *
+ *                                                                         *
+ *   Project Euler (TRIGEMEO PITAGORICO ESPECIAL)                          *
+ *   Copyright (C) 2015-2019   Template by Ruben Carlo Benante             *
+ *   Author: Leandro Dantas Lima                                           *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Leandro Dantas Lima                                                   *
+ *   Email: leandroautocontrole@gmail.com                                  *
+ *   Webpage: http://beco.poli.br/leandroautocontrole@gmail.com            *
+ *   Phone: +55 (81) 98861-9469                                            *
+ **************************************************************************/
+
+/* ---------------------------------------------------------------------- */
+/* includes */
+#include <stdio.h>
+#include <stdlib.h>
+/* ---------------------------------------------------------------------- */
+/* ---------------------------------------------------------------------- */
+/* main function */
+
+int main(void)
+{
+ 
+    return EXIT_SUCCESS;
+}
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
+
